cb2f5453c58111917308b35477643533ac9a333c
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2024-07-18 12:27:13 +0000

    datatypes variables config

---
+++ 01_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict";  // treat all js code as newer version
+//alert(3+3)// we are using nodejs,not browser
+console.log(3+
+    3);  // code readibility should be high
+    console.log("rashi")
+    let name="rashi"
+    let age = 18
+    let isLoggdIn = false
+    let state;
+    // number => 2 to power 53
+    //bigint
+    // string =>""
+    //boolean => true/false
+    //null => standlone value
+    //undefined
+    // symbol => unique
+    // object
+    console.log(typeof null)// return object
+    console.log(typeof null)// return undefined
---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="rashi@123gmail.com"
+var accountPassword = "12345"
+accountCity = "gaziabad"
+let accountState;  // undefined
+// accounId=2
+/*
+prefer not to use var because of the issue of block scope and functional scope
+*/
+accountEmail="divyansh@134gmail.com"
+accountPassword="58768"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
