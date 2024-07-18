8ca32ed6f20d37e6cb69efc5314b0a12524af731
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2024-07-18 13:05:45 +0000

    Conversion of datatypes

---
+++ 01_conversionandoperation.js
@@ -0,0 +1,19 @@
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+// "33" => 33
+//"33abc" => NAN
+// true => 1; false=>0
+// null => object
+// let isloggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1=> true; 0=>false
+//"" => false
+// "rashi" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
