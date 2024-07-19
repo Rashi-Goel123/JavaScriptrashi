cc7434e39186ef0cc1440f3936e60f094abc14af
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2024-07-19 10:09:23 +0000

    changes config done

---
+++ 01_basics/01_conversionandoperation.js
@@ -0,0 +1,51 @@
+// let score=undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+// "33" => 33
+//"33abc" => NAN
+// true => 1; false=>0
+// null => object
+// let isloggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1=> true; 0=>false
+//"" => false
+// "rashi" => true
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+//******************operations********************//
+let value =3
+    let negvalue= -value
+    // console.log(2*2);
+    // console.log(2-2);
+    // console.log(2+2);
+    // console.log(2**2);
+    // console.log(2/3);
+    // console.log(2%2);
+    let str1="hello"
+    let str2="rashi"
+    let str3=str1+str2
+    // console.log(str3)
+    // console.log("1" + 2);
+    // console.log(1+"2");
+    // console.log("1"+2+2);
+    // console.log(1+2+"2");
+    // console.log(negvalue)
+  console.log(+true);
+  let num1,num2,num3=2+2
+  let  gameCounter=100
+  gameCounter++;
+  console.log(gameCounter++);
+  console.log("a">1); 
+  console.log(null>0);
+  console.log(null==0);
+  console.log(null>=0);
+  console.log(undefined==0);
+  console.log(undefined>=0);
+  console.log(undefined<=0);
+// === also check the datatypes//
+console.log("2"==1);
---
+++ 01_basics/01_datatypessummary.js
@@ -0,0 +1,36 @@
+//7 data types:string,number,bean,nu,undefined,symbol,bigint//
+const score = 100
+const scorevalue= 100.3
+const isloggedIn =false
+const outsidetemp =null
+let useremail;
+const id = symbol('123')
+const accountid=symbol('123')
+const bignumber=3456543665687987986n
+const heroes =["rashi","divyansh","tejas"]
+let myobj ={
+name:"rashi",
+age:20,
+}
+const myfunction=function(){
+    console.log("hello rashi")
+}
+//primitive datatypes
+// number=>number
+//string=> string
+//boolean => boolean
+//null=>object
+//undefined=>undefined
+//symbol=>symbol
+//bigint=>bigint
+//reference (non) primitive
+//array,objects,functions
+//array=>object
+//functions=>functions
+//object=>object
+//***************************************//
+//primitive (stack)
+// non primitive (heap)
+let myyoutubename = "rashi"
+let anothername = "myyoutubename"
+anothername="chaiaurcode"
