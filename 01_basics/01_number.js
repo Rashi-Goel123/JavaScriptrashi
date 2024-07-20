8243515b1a8e249d1b90ed89b916d16f3c25e2a1
Rashi-Goel123 <161209446+Rashi-Goel123@users.noreply.github.com>
2024-07-20 09:40:48 +0000

    number config done

---
+++ 01_basics/01_number.js
@@ -0,0 +1,27 @@
+// const score =100
+// console.log(score)
+// const balance=new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+// const othernumber =1123.8966
+// console.log(othernumber.toPrecision(5))
+// let hundred = 100000000;
+// console.log(hundred.toLocaleString('en-IN'));
+//********************************************maths***************************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));//upper
+console.log(Math.floor(4.9));//lower
+console.log(Math.min(4,6,9,7));
+console.log(Math.max(4,6,9,7));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+
+
+
+
